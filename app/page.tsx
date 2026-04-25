0588c79e8ec5a176047a070bc7e445e35ad65f25
kashif <kashusays1@gmail.com>
2026-04-25 11:07:08 +0500

    page

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,16 @@
-import Image from "next/image";
+import { Metadata } from "next";
+import ServicesClient from "@/services/ServicesClient";
 
-export default function Home() {
-  return (
-    <div className="min-h-screen bg-light-gray">
-    </div>
-  );
+export const metadata: Metadata = {
+  title:
+    "Engine Repair & Car Services in Baku | Suspension, Brakes, Oil Change & More",
+  description:
+    "Get expert engine repair in Baku along with suspension services, brake repair, battery replacement, tire services, AC service, oil change, and car diagnostics. Reliable auto service in Baku for all your vehicle needs.",
+  other: {
+    "google-site-verification": "iACxYXX2j0rTzino0ftnhhHgfudsBSIR743gyw9uOEg",
+  },
+};
+
+export default function Page() {
+  return <ServicesClient />;
 }
